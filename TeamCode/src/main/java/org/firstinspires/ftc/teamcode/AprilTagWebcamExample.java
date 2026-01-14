fc3c47aff21e579383aabb541b8be07dc4d51d9e
mdimascio-lab <mdimascio@ash.nl>
2026-01-14 08:51:52 +0100

    April tag code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+public class AprilTagWebcamExample extends OpMode {
+
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+
+
+    @Override
+    public void init() {
+        aprilTagWebcam.init(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        //update the vision portal
+        aprilTagWebcam.update();
+        AprilTagDetection id20 = aprilTagWebcam.getTagSpecificID(20);
+        telemetry.addData("id20 String", id20.toString());
+    }
+}
