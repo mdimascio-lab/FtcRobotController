4975535e2f9813ef9d77d5404605d8ba6fa7baa2
baquwerdz <dropthememes@gmail.com>
2026-01-19 18:15:58 +0100

    small thhings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAuto.java
@@ -1,6 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 
-public class TeleOpAuto extends OpMode {
-}
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+//@Disabled
+//@TeleOp
+//@Autonomous
+//public class TeleOpAuto extends OpMode {
+//}
