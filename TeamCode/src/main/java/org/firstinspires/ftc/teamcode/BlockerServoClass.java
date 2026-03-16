a833587bceb464eabd1e4a13898f9457443ab73b
baquwerdz <dropthememes@gmail.com>
2026-03-16 21:18:40 +0100

    finished the servo code
    need to configure it to the robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlockerServoClass.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.BlockerServo;
+
+@TeleOp
+public class BlockerServoClass extends OpMode{
+    BlockerServo blocker = new BlockerServo();
+    @Override
+    public void init() {
+        blocker.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            blocker.setServoPos(0); // full left. mid point 0.5
+        }
+        else {
+            blocker.setServoPos(1.0); // full right
+        }
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/BlockerServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/BlockerServo.java
@@ -8,5 +8,11 @@ public class BlockerServo {
 
     public void init(HardwareMap hwMap) {
         servoPos = hwMap.get(Servo.class, "servo_pos");
+        servoPos.scaleRange(0.5,1.0); //set range from midpoint to 180 degrees, rather than allwoing the servo to go back to 0 degrees
+        //servoPos.setDirection(Servo.Direction.REVERSE); // reverses the direction
+    }
+
+    public void setServoPos(double angle) {
+        servoPos.setPosition(angle);
     }
 }
