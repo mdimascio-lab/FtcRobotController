f130db3bef481a7eb0472f6f6b82427b940b3a8f
baquwerdz <dropthememes@gmail.com>
2026-03-18 13:23:12 +0100

    small servo changes
    need to make it a bit better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlockerServoClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlockerServoClass.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.BlockerServo;
@@ -16,7 +16,7 @@ public class BlockerServoClass extends OpMode{
     @Override
     public void loop() {
         if (gamepad1.a) {
-            blocker.setServoPos(0); // full left. mid point 0.5
+            blocker.setServoPos(0.5); // full left. mid point 0.5
         }
         else {
             blocker.setServoPos(1.0); // full right
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NEWLATESTTELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NEWLATESTTELEOP.java
@@ -1,14 +1,16 @@
-package org.firstinspires.ftc.teamcode;
-
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.teamcode.mechanisms.Intake;
-import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
-import org.firstinspires.ftc.teamcode.mechanisms.Shooter;
-import org.firstinspires.ftc.teamcode.mechanisms.Transfer;
-
-public class NEWLATESTTELEOP extends OpMode {
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//import org.firstinspires.ftc.teamcode.mechanisms.Intake;
+//import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+//import org.firstinspires.ftc.teamcode.mechanisms.Shooter;
+//import org.firstinspires.ftc.teamcode.mechanisms.Transfer;
+//
+//@Disabled
+//public class NEWLATESTTELEOP extends OpMode {
+//}
